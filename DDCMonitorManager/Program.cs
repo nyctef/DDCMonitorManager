70b1d0bade59adf46850e5a7439e0ca0e15ff5b3
Nyctef <nyctef@nyctef.com>
2019-04-20 05:05:53 +0100

    make program entry point explicit

---
+++ DDCMonitorManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DDCMonitorManager
+{
+    public static class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            if (!args.Any())
+            {
+                new Application().Run(new MainWindow());
+            }
+            return 0;
+        }
+    }
+}
